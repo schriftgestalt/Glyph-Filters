a1ac9f7c6a635e59c31c7fb5fc544b12f4019bb9
schriftgestalt <georg.seifert@mac.com>
2025-01-19 16:00:51 +0100

    use tabs

--- NaNGlyphFilters/80sFade.py
+++ NaNGlyphFilters/80sFade.py
@@ -6,71 +6,76 @@ __doc__ = """
 
 from NaNGlyphsEnvironment import GSComponent
 from NaNGFGraphikshared import (
-    setGlyphCoords,
-    AllPathBounds,
-    MakeRectangles,
-    ClearPaths,
-    AddAllComponentsToLayer,
-    CreateAllShapeComponents,
-    withinGlyphBlack,
-    point_inside_polygon,
+	setGlyphCoords,
+	AllPathBounds,
+	MakeRectangles,
+	ClearPaths,
+	AddAllComponentsToLayer,
+	CreateAllShapeComponents,
+	withinGlyphBlack,
+	point_inside_polygon,
 )
+import importlib
+import NaNGFAngularizzle
+importlib.reload(NaNGFAngularizzle)
+
 from NaNGFAngularizzle import ConvertPathsToSkeleton
 from NaNFilter import NaNFilter
 import random
 
 
 class EightiesFade(NaNFilter):
-    def processLayer(self, thislayer, params):
-        pathlist = ConvertPathsToSkeleton(thislayer.paths, 20)
-        outlinedata = setGlyphCoords(pathlist)
+	def processLayer(self, thislayer, params):
+		pathlist = ConvertPathsToSkeleton(thislayer.paths, 20)
+		print("__pathlist", type(pathlist))
+		outlinedata = setGlyphCoords(pathlist)
 
-        try:
-            startrect = AllPathBounds(thislayer)
-            allrectangles = MakeRectangles([startrect], 5)
-            shape_components = CreateAllShapeComponents(self.font, 100, 100)
+		try:
+			startrect = AllPathBounds(thislayer)
+			allrectangles = MakeRectangles([startrect], 5)
+			shape_components = CreateAllShapeComponents(self.font, 100, 100)
 
-            fadecomps = []
+			fadecomps = []
 
-            for n in range(0, len(allrectangles)):
-                x, y, w, h = allrectangles[n]
-                tilecoords = [[x, y], [x, y + h], [x + w, y + h], [x + w, y]]
-                fadecomps.extend(
-                    self.do80sFade(thislayer, outlinedata, tilecoords, shape_components)
-                )
+			for n in range(0, len(allrectangles)):
+				x, y, w, h = allrectangles[n]
+				tilecoords = [[x, y], [x, y + h], [x + w, y + h], [x + w, y]]
+				fadecomps.extend(
+					self.do80sFade(thislayer, outlinedata, tilecoords, shape_components)
+				)
 
-            ClearPaths(thislayer)
-            AddAllComponentsToLayer(fadecomps, thislayer)
+			ClearPaths(thislayer)
+			AddAllComponentsToLayer(fadecomps, thislayer)
 
-        except Exception as e:
-            print(("Layer (", thislayer.name, ") failed to execute.", e))
+		except Exception as e:
+			print(("Layer (", thislayer.name, ") failed to execute.", e))
 
-    def do80sFade(self, thislayer, outlinedata, tilecoords, shape_components):
-        b = AllPathBounds(thislayer)
+	def do80sFade(self, thislayer, outlinedata, tilecoords, shape_components):
+		b = AllPathBounds(thislayer)
 
-        if b is None:
-            return []
+		if b is None:
+			return []
 
-        fadecomps = []
-        size = random.randrange(4, 15)
-        r = random.choice(shape_components)
-        ox, oy, w, h = b[0], b[1], b[2], b[3]
+		fadecomps = []
+		size = random.randrange(4, 15)
+		r = random.choice(shape_components)
+		ox, oy, w, h = b[0], b[1], b[2], b[3]
 
-        for y in range(oy, oy + h, 20):
-            for x in range(ox, ox + w, 20):
-                if (
-                    withinGlyphBlack(x, y, outlinedata)
-                    and point_inside_polygon(x, y, tilecoords)
-                    and size > 2
-                ):
-                    fadecomp = GSComponent(r)
-                    scale = size / 100.0
-                    fadecomp.scale = (scale, scale)
-                    fadecomp.position = (x, y)  # ?
-                    fadecomps.append(fadecomp)
-                size += 0.01
+		for y in range(oy, oy + h, 20):
+			for x in range(ox, ox + w, 20):
+				if (
+					withinGlyphBlack(x, y, outlinedata)
+					and point_inside_polygon(x, y, tilecoords)
+					and size > 2
+				):
+					fadecomp = GSComponent(r)
+					scale = size / 100.0
+					fadecomp.scale = (scale, scale)
+					fadecomp.position = (x, y)  # ?
+					fadecomps.append(fadecomp)
+				size += 0.01
 
-        return fadecomps
+		return fadecomps
 
 
 EightiesFade()
